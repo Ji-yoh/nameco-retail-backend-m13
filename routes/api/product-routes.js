7fc76888c89ec2b73a3bb4f0a34cf19d9016a22b
Jiyoh Macbook <geovannih514@gmail.com>
2023-09-06 12:18:17 -0700

    added category routes, still working on product and tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    })
+    res.status(200).json(productData)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json('Unexpected Error')
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = Product.findByPk(req.params.id, {
+      include:[{ model: Category }, { model: Tag }]
+    })
+    if (!productData) {
+      res.status(404).json('Product does not exist')
+    }
+    res.status(200).json(productData)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json('Unexpected Error')
+  }
 });
 
 // create new product
